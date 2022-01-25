d39d30fe5a5711bad26c0e63272c3c28d2e10ed1
heeyun <48400833+yunniez@users.noreply.github.com>
2022-01-25 21:17:27 +0900

    Create heeyun.java

---
+++ programmers/콜라츠추측/heeyun.java
@@ -0,0 +1,25 @@
+public class S78 {
+	public static void main(String[] args) {
+		double num = 626331;
+		System.out.println(solution(num));
+	}
+	
+	public static int solution(double num) {
+        int answer = 0;
+        
+        while(num != 1){
+            if(num%2 == 0){
+	           num /= 2;
+            }else if(num%2 == 1){
+               num = (num*3) + 1;
+            }
+
+            answer++;
+            if(answer > 500){
+                answer = -1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
